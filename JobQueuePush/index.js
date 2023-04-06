0fbc68e03bdba5a40026dfa932be68aa7f696ae7
fjgao <fjgao@bu.edu>
2023-04-06 17:26:39 -0400

    return sth when http triggered

--- JobQueuePush/index.js
+++ JobQueuePush/index.js
@@ -1,4 +1,7 @@
 module.exports = async function (context, input) {
     context.bindings.myQueueItem = ["input.body","test"]; // for testing purpose only. Use the following line for real.
     // context.bindings.myQueueItem = input.body;
+    return {
+        body: "things pushed to queue"
+    };
 };
