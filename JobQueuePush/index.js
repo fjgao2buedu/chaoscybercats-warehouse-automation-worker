e6d3323205f632fe4417a9d2c2989e725efe3567
fjgao <fjgao@bu.edu>
2023-04-06 15:26:47 -0400

    httptrigger and queuetrigger

---
+++ JobQueuePush/index.js
@@ -0,0 +1,4 @@
+module.exports = async function (context, input) {
+    context.bindings.myQueueItem = ["input.body","test"]; // for testing purpose only. Use the following line for real.
+    // context.bindings.myQueueItem = input.body;
+};
