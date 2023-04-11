5c94675bb92a8691ccc61762dfede35054d38178
fjgao <fjgao@bu.edu>
2023-04-11 17:40:35 -0400

    display the input image url for testing

--- JobQueuePush/index.js
+++ JobQueuePush/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, input) {
-    context.bindings.myQueueItem = ["input.body","test"]; // for testing purpose only. Use the following line for real.
+    context.bindings.myQueueItem = [input.query.imgurl]; // for testing purpose only. Use the following line for real.
     // context.bindings.myQueueItem = input.body;
     return {
-        body: "things pushed to queue"
+        body: input.query.imgurl
     };
 };
