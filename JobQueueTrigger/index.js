9032772b55f65440139851bd1eca7f37727cc325
fjgao <fjgao@bu.edu>
2023-04-11 17:44:51 -0400

    queue trigger put api result to database

--- JobQueueTrigger/index.js
+++ JobQueueTrigger/index.js
@@ -1,3 +1,20 @@
+const fetch = require('node-fetch')
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    const data = { url: myQueueItem };
+    const recognizedData = await fetch("https://infinitecats.cognitiveservices.azure.com/computervision/imageanalysis:analyze?features=caption,read&model-version=latest&language=en&api-version=2023-02-01-preview", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': process.env.Ocp_Apim_Subscription_Key
+        },
+    })
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    // .catch(error => console.error(error));
+
+    context.bindings.outputDocument = recognizedData;
+    
+    context.log('Document parsed: \n', recognizedData);
 };
