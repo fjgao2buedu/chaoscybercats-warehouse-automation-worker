a32c918b09290a8070225ff4614f5a9575104aa7
fjgao <fjgao@bu.edu>
2023-04-14 01:20:15 -0400

    comments

--- JobQueueTrigger/index.js
+++ JobQueueTrigger/index.js
@@ -1,11 +1,15 @@
 const fetch = require('node-fetch')
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    const data = { urlSource: myQueueItem };
+    // computer vision resource
     const computer_vision = "https://infinitecats.cognitiveservices.azure.com/computervision/imageanalysis:analyze?features=caption,read&model-version=latest&language=en&api-version=2023-02-01-preview";
+    // form recognizer resource
     const form_recognizer = "https://infinitechaoscats.cognitiveservices.azure.com/formrecognizer/documentModels/prebuilt-read:analyze?api-version=2022-08-31";
-    
-    const analyzeOperation = await fetch(form_recognizer, {
+
+    // data input for form recog
+    const data = { urlSource: myQueueItem };
+    // submitting job to analyze, return operation-location url
+    const operation_location = await fetch(form_recognizer, {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
@@ -13,24 +17,28 @@ module.exports = async function (context, myQueueItem) {
             'Ocp-Apim-Subscription-Key': process.env.Form_Recognizer_Ocp_Apim_Subscription_Key
         },
     })
-    .then(response => response.headers.get('operation-location'))
-    // .then(data => console.log(data))
-    // .catch(error => console.error(error));
+        .then(response => response.headers.get('operation-location'))
+        .catch(error => console.error(error));
+    context.log('operation-location:', operation_location)
 
-    // const analyzeOperation = "https://infinitechaoscats.cognitiveservices.azure.com/formrecognizer/documentModels/prebuilt-read/analyzeResults/5e3a1880-306c-4b80-90e3-2820dba63041?api-version=2022-08-31";
-    context.log('Get response from:', analyzeOperation )
+    // fetch until job complete
+    do {
+        var result = await fetch(operation_location, {
+            method: 'GET',
+            headers: {
+                'Ocp-Apim-Subscription-Key': process.env.Form_Recognizer_Ocp_Apim_Subscription_Key
+            },
+        })
+            .then(response => response.json());
+        await new Promise(r => setTimeout(r, 1000));
+    } while (result.status !== "succeeded")
+    context.log('operation status:', result.status);
 
-    await new Promise(r => setTimeout(r, 5000));
-    const result = await fetch(analyzeOperation, {
-        method: 'GET',
-        headers: {
-            'Ocp-Apim-Subscription-Key': process.env.Form_Recognizer_Ocp_Apim_Subscription_Key
-        },
-    })
-        .then(response => response.json())
+    // parse analyzeResult to lines of words
     const lines = result.analyzeResult.pages.map(page => page.lines.map(line => line.content.split(":").map(part => part.trim()))).reduce((acc, list) => acc.concat(list));
-    // context.log('lines: \n', lines )
+    context.log('lines: \n', lines )
 
+    // begin parse document to key value pairs
     const parsedContent = {};
     let attr_to_combine_multiple_paragraph = lines[0][0];
     lines.forEach((parts) => {
@@ -43,25 +51,6 @@ module.exports = async function (context, myQueueItem) {
     });
     context.log('Document parsed: \n', parsedContent );
 
+    // write to database
     context.bindings.outputDocument = parsedContent;
-    // =================
-
-    // context.log('Document parsed: \n', recognizedData);
-
-    // const data = { url: myQueueItem };
-    // const recognizedData = await fetch("https://infinitecats.cognitiveservices.azure.com/computervision/imageanalysis:analyze?features=caption,read&model-version=latest&language=en&api-version=2023-02-01-preview", {
-    //     method: 'POST',
-    //     body: JSON.stringify(data),
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Ocp-Apim-Subscription-Key': process.env.Ocp_Apim_Subscription_Key
-    //     },
-    // })
-    // .then(response => response.json())
-    // // .then(data => console.log(data))
-    // // .catch(error => console.error(error));
-
-    // context.bindings.outputDocument = recognizedData;
-
-    // context.log('Document parsed: \n', recognizedData);
 };
