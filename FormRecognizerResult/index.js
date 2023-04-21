d9901635c0cf8235d93b97ca4967785e0f978867
fjgao <fjgao@bu.edu>
2023-04-21 15:43:57 -0400

    return message if file not found

--- FormRecognizerResult/index.js
+++ FormRecognizerResult/index.js
@@ -1,7 +1,12 @@
 module.exports = async function (context, input) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    var responseMessage = context.bindings.fileResult[0];
+    var response = context.bindings.fileResult
+
+    // if (!response.length) { return { body: [{"content":"empty"}] } }
+    if (!response.length) { return { body: { warning: "file not found, check filename or try again in 30 seconds." } } }
+
+    var responseMessage = response[0];
     for (let key in responseMessage) {
         if (key.match(/^\_/)) {
             delete responseMessage[key];
